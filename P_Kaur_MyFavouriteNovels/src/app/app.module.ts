71e8663e4a45f06628ad65f1f1a96beb9e790903
PalwinderK <w0771480@myscc.ca>
2022-02-20 20:49:39 -0500

    assignment4

---
+++ P_Kaur_MyFavouriteNovels/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
+import { HoverAffectDirective } from './hover-affect.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentCardComponent,
+    ContentListComponent,
+    HoverAffectDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ P_Kaur_MyFavouriteNovels/src/app/content-list/content-list.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.scss'],
+})
+export class ContentListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public myFavNovels = [
+    {
+      id: 0,
+      title: "Gulliver's Travels",
+      description: 'A satirical masterpiece that never been out',
+      creator: 'Jonathan Swift',
+      imgURL:
+        'https://cdn.britannica.com/90/125990-050-F5E91F84/Lemuel-Gulliver-edition-illustration-Lilliput-Gullivers-Travels.jpg',
+    },
+    {
+      id: 1,
+      title: "The Pilgrim's Progress",
+      description: 'A story of man in search of truth.',
+      creator: 'John Bunyan',
+      type: 'classic',
+    },
+    {
+      id: 2,
+      title: 'Tom Jones',
+      description: 'It is a classic novel that captures spirit of its age.',
+      creator: 'Henry Fielding',
+      imgURL:
+        'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1180342959l/1028649.jpg',
+      type: 'classic',
+    },
+    {
+      id: 3,
+      title: 'Where the Wild Ladies Are',
+      description:
+        'In Where the Wild Ladies Are, Japanese author Aoko Matsuda guides readers through supernatural events and introduces them to otherworldly characters as if they were completely ordinary.',
+      creator: 'Aoko Matsuda',
+      imgURL:
+        'https://api.time.com/wp-content/uploads/2020/11/best-books-2020-Where-the-Wild-Ladies-Are.jpg?w=800&quality=85',
+      type: 'classic',
+    },
+    {
+      id: 4,
+      title: 'A Burning, Megha Majumdar',
+      description:
+        'After witnessing a terrorist attack, Jivan, a poor Muslim woman living in the slums of Kolkata, makes a comment on Facebook criticizing her government’s response to the tragic event. It’s an action with terrible consequences, as she’s taken into custody and accused of aiding the attackers.',
+      imgURL:
+        'https://api.time.com/wp-content/uploads/2020/11/best-books-2020-Burning.jpg?w=800&quality=85',
+      type: 'classic',
+    },
+    {
+      id: 5,
+      title: 'Homeland Elegies, Ayad Akhtar',
+      description:
+        'Every so often we are gifted a novel that combines deep intelligence, meticulous prose and something profound to say about the state of our world.',
+      imgURL:
+        'https://d3i5mgdwi2ze58.cloudfront.net/phz3vc780qvnafu8phrzjyptq75a',
+      type: 'classic',
+    },
+    {
+      id: 6,
+      title: 'Deacon King Kong, James McBride',
+      description:
+        'It’s September 1969 when Sportcoat, the grumpy old deacon of a church in the Causeway Houses project in Brooklyn, shoots local drug dealer Deems in the face. ',
+      imgURL:
+        'https://api.time.com/wp-content/uploads/2020/11/best-books-2020-Deacon-King-Kong.jpg?w=800&quality=85',
+      type: 'classic',
+    },
+
+    {
+      id: 7,
+      title: 'The Mirror & the Light, Hilary Mantel',
+      description:
+        'The Mirror & The Light lays out the downfall of Thomas Cromwell, consigliere to King Henry VIII and powerbroker of the Reformation. It’s historical fiction, but dazzlingly literary in its ambitions and dramatic in the cut and thrust of its dialogue.',
+      imgURL:
+        'https://api.time.com/wp-content/uploads/2020/11/best-books-2020-Mirror-and-the-Light.jpg?w=800&quality=85',
+      type: 'classic',
+    },
+  ];
+
+  showNovalInfo(id: any, title: String) {
+    console.log({ id, title });
+  }
+}

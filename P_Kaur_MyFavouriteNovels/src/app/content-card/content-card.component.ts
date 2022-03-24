d95e825b75e10b87a1f8f63b249cd99f04196ee3
PalwinderK <w0771480@myscc.ca>
2022-03-24 06:02:36 -0400

    assignment-2

---
+++ P_Kaur_MyFavouriteNovels/src/app/content-card/content-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.css'],
+})
+export class ContentCardComponent implements OnInit {
+  ngOnInit(): void {}
+}
